fe5fca694879899c2db541146b227ade39b5ab9d
VISHNU V S <84698110+vishnuvskvkl@users.noreply.github.com>
2022-04-01 09:31:16 +0530

    Update and rename k-means clustering .py to k-means clustering(using scikit-learn) .py

--- k-means clustering(using scikit-learn) .py
+++ k-means clustering(using scikit-learn) .py
@@ -5,6 +5,18 @@ iris=load_iris()
 df=pd.DataFrame(iris.data,columns=iris.feature_names)
 x=df.iloc[:,0:2]
 y=iris.target
+#finding the number of clusters using elbow-method
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,11),wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+print(wcss)
 km=KMeans(n_clusters=3, init='k-means++', n_init=10, max_iter=300, random_state=0)
 y_km=km.fit_predict(x)
 #displaying the cluster centers
@@ -17,5 +29,5 @@ plt.scatter(x.iloc[y_km==2,0],x.iloc[y_km==2,1],s=100,c='purple',label='Iris-vir
 plt.scatter(km.cluster_centers_[:,0],km.cluster_centers_[:,1],s=200,c='yellow',label='Centroids')
 plt.legend()
 plt.show()
-#print the number of steps to stabilize the model
+#number of steps to stabilize the model
 print(km.n_iter_)
